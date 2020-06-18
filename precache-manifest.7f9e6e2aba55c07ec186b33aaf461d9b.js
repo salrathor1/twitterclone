ab69022fe4d0b81f6efb86e67065615b6cc45b93
salrathor1 <salrathor1@hotmail.com>
2020-06-18 15:36:58 +0100

    Updates

---
+++ precache-manifest.7f9e6e2aba55c07ec186b33aaf461d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75af396233b8619d4e16902ad41fcb8a",
+    "url": "/twitterclone/index.html"
+  },
+  {
+    "revision": "ec0bca687875fd75201e",
+    "url": "/twitterclone/static/css/main.d84d6a9c.chunk.css"
+  },
+  {
+    "revision": "11a3a9131b286742703f",
+    "url": "/twitterclone/static/js/2.acf09f92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twitterclone/static/js/2.acf09f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0bca687875fd75201e",
+    "url": "/twitterclone/static/js/main.a9a59eb5.chunk.js"
+  },
+  {
+    "revision": "9d5a85be51583b5d19a9",
+    "url": "/twitterclone/static/js/runtime-main.68fc7b6b.js"
+  }
+]);
